55432821074b79987f57ff5d2c55ac8f68dd3a44
Tim Vergenz <vergenzt@gmail.com>
2023-11-26 23:37:08 -0500

    wip change to ContentView.swift

--- shortcuts-http-client/ContentView.swift
+++ shortcuts-http-client/ContentView.swift
@@ -3,10 +3,18 @@ import SwiftUI
 struct ContentView: View {
   var body: some View {
     VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundStyle(.tint)
-      Text("Hello, world!")
+      Spacer()
+      
+      Text("Hello, Shortcuts developer!")
+        .font(.largeTitle)
+        .multilineTextAlignment(.center)
+      
+      Spacer()
+      
+      Text("Open the Shortcuts app to view this app's shortcuts.")
+        .multilineTextAlignment(.center)
+      
+      Spacer()
     }
     .padding()
   }
